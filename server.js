7b58188b1dac5da9f463b1bdd5f71660f5bf7eeb
Tsara Sudrajat <tsara.tf@gmail.com>
2022-02-03 17:34:16 +0700

    Add json response

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -20,11 +20,21 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  let headers = req.headers;
+
+  res.json({
+    ipaddress: req.ip,
+    language: headers["accept-language"],
+    software: headers["user-agent"]
+
+  });
+});
 
 
 // listen for requests :)
